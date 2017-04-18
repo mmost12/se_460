7461677de49f7574824ddd545cc6efa61e8d6df4
Micah Most <micahmost12@gmail.com>
2017-04-17 20:23:27 -0500

    Saving changes to Workout before adding JFrame.

--- Workout.java
+++ Workout.java
@@ -1,222 +1,342 @@
 
 import java.util.*;
 import javax.swing.JFrame;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.util.concurrent.TimeUnit;
+
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.Timer;
+import javax.swing.SwingConstants;
+import java.text.*;
 
 public class Workout {
-   String wo_name;
    Date wo_date;
-   int cals_burned;
    float watts_made;
-   int avg_hbm;
    float wo_distance;
-   int wo_duration;
+   int avg_hbm;
+   int cals_burned;
    int resistence_level;
-   Workout_timer wo_timer;
+   String wo_name;
+   Workout_manager wo_manager;
 
 
-   public Workout(String name) {
+   // -------------------------
+   //  ClockLabel dateLable = new ClockLabel("date");
+   //  ClockLabel timeLable = new ClockLabel("time");
+   //  ClockLabel dayLable = new ClockLabel("day");
+    //
+   //  JFrame f = new JFrame("Digital Clock");
+   // ------------------------
+
+   public Workout() {
       // This constructor has one parameter, name.
-      System.out.println("Name chosen is :" + name );
-      wo_name          = name;
+      wo_name          = "Manual";
       wo_date          = new Date();
       cals_burned      = 0;
       watts_made       = 0;
       avg_hbm          = 0;
       wo_distance      = 0;
-      wo_duration      = 0;
-      resistence_level = 1;
-      wo_timer         = new Workout_timer();
+      resistence_level = 0;
+      wo_manager       = new Workout_manager();
 
-      wo_timer.setDaemon(true);
+      // ------------
+      //  JFrame.setDefaultLookAndFeelDecorated(true);
+      //  f.setSize(300,150);
+      //  f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      //  f.setLayout(new GridLayout(3, 1));
+       //
+      //  f.add(dateLable);
+      //  f.add(timeLable);
+      //  f.add(dayLable);
+       //
+      //  f.getContentPane().setBackground(Color.black);
+       //
+      //  f.setVisible(true);
+      // ------------
+
+      // wo_manager.setDaemon(true);
    }
 
    public void start_wo() {
-      System.out.println("Begin workout!");
-      wo_timer.start();
-   }
+      Scanner reader = new Scanner(System.in);
 
-   public void shout_out() {
-      wo_timer.speak();
-   }
+      System.out.print("Select resistence level: (1-20)\n-> ");
+      resistence_level = reader.nextInt();
 
-   public void finish() {
-      int total_time = wo_timer.get_time();
-
-      System.out.println("Workout summary:\n" +
-                         "Time:     " + total_time + "\n" +
-                         "Diff Lvl: " + resistence_level);
-   }
-
-   public Boolean check_finished() {
-      int current_time = wo_timer.get_time();
-
-      if(current_time < wo_duration) {
-         return true;
+      try {
+         System.out.print("3.. ");
+         Thread.sleep(1000);
+         System.out.print("2.. ");
+         Thread.sleep(1000);
+         System.out.print("1.. ");
+         Thread.sleep(1000);
+      } catch (InterruptedException e) {
+         e.printStackTrace();
       }
-      else {
-         return false;
+      System.out.print("\rBegin workout!\n");
+
+      wo_manager.set_resistence(resistence_level);
+      // wo_manager.start();
+   }
+
+   public void run_wo() {
+      Boolean keep_going = true;
+      Scanner reader     = new Scanner(System.in);
+
+      while(keep_going) {
+
+         resistence_level = reader.nextInt();
+
+         if(resistence_level == 0) {
+            keep_going = false;
+         }
+         else {
+            wo_manager.set_resistence(resistence_level);
+         }
       }
    }
 
-// ---------------------------- setter functions ---------------------------- //
-   public void set_wo_name( String name ) {
-      wo_name = name;
+   public void finish_wo() {
+      int total_time       = wo_manager.get_time();
+      float total_distance = wo_manager.get_distance();
+      String display_time;
+      // wo_manager.interrupt();
+
+      display_time = String.format("%2d:%2d",
+         TimeUnit.SECONDS.toMinutes(total_time),
+         total_time - TimeUnit.SECONDS.toMinutes(total_time) * 60
+      );
+
+      System.out.print("\r\033[K\n-------------------------------------------------\n" +
+                       "Workout summary:     " + wo_date + "\n\n");
+      System.out.format("  Distance:   %.2f\n" +
+                        "  Time:       %s\n" +
+                        "  Diff Lvl:   %d\n" +
+                        "  Calories:   %d\n" +
+                        "  Watts:      %f\n" +
+                        "  Heart rate: %d\n" +
+                        "-------------------------------------------------\n",
+                        total_distance, display_time, resistence_level, cals_burned, watts_made, avg_hbm);
    }
 
-   public void set_wo_date( Date date ) {
-      wo_date = date;
-   }
+// ----------------------------- setter methods ----------------------------- //
+   public void set_wo_name( String name ) {wo_name = name;}
+   public void set_wo_date( Date date ) {wo_date = date;}
+   public void set_cals_burned( int cals ) {cals_burned = cals;}
+   public void set_watts_made( float watts ) {watts_made = watts;}
+   public void set_avg_hbm( int hbm ) {avg_hbm = hbm;}
+   public void set_distance( float distance ) {wo_distance = distance;}
+   public void set_resistence( int resistence ) {resistence_level = resistence;}
+// -------------------------- End setter methods ---------------------------- //
 
-   public void set_cals_burned( int cals ) {
-      cals_burned = cals;
-   }
-
-   public void set_watts_made( float watts ) {
-      watts_made = watts;
-   }
-
-   public void set_avg_hbm( int hbm ) {
-      avg_hbm = hbm;
-   }
-
-   public void set_distance( float distance ) {
-      wo_distance = distance;
-   }
-
-   public void set_duration( int duration ) {
-      wo_duration = duration;
-   }
-
-   public void set_resistence_level( int resistence ) {
-      resistence_level = resistence;
-   }
-// ------------------------- End setter functions --------------------------- //
-
-// ---------------------------- getter functions ---------------------------- //
-   public String get_wo_name() {
-      System.out.println("Workout name: " + wo_name);
-      return wo_name;
-   }
-
-   public Date get_wo_date() {
-      System.out.println("Workout date: " + wo_date);
-      return wo_date;
-   }
-
-   public int get_cals_burned() {
-      System.out.println("Calories burned: " + cals_burned);
-      return cals_burned;
-   }
-
-   public float get_watts_made() {
-      System.out.println("Watts generated: " + watts_made);
-      return watts_made;
-   }
-
-   public int get_avg_hbm() {
-      System.out.println("Average heart rate: " + avg_hbm);
-      return avg_hbm;
-   }
-
-   public float get_distance() {
-      System.out.println("Distance: " + wo_distance);
-      return wo_distance;
-   }
-
-   public int get_duration() {
-      System.out.println("Duration: " + wo_duration);
-      return wo_duration;
-   }
-
-   public int get_resistence_level() {
-      System.out.println("Resistence level: " + resistence_level);
-      return resistence_level;
-   }
-// ------------------------- End getter functions --------------------------- //
-
-// finish()
-// pause()
-// reset()
-
+// ----------------------------- getter methods ----------------------------- //
+   public String get_wo_name() {return wo_name;}
+   public Date get_wo_date() {return wo_date;}
+   public int get_cals_burned() {return cals_burned;}
+   public float get_watts_made() {return watts_made;}
+   public int get_avg_hbm() {return avg_hbm;}
+   public float get_distance() {return wo_distance;}
+   public int get_resistence_level() {return resistence_level;}
+// -------------------------- End getter methods ---------------------------- //
 
    public static void main(String []args) {
 
-      Workout wo1    = new Workout( "Workout #1" );
-      Scanner reader = new Scanner(System.in);
       int choice;
-      int duration;
-      int difficulty;
+      Boolean continue_wo = true;
+      Workout wo          = new Workout();
+      Scanner reader      = new Scanner(System.in);
 
       System.out.println("Please select a workout mode from the list:\n" +
-                         "0 - Manual\n" +
-                         "1 - Hill\n" +
-                         "2 - Cardio\n" +
-                         "3 - Strength");
+                         "1 - Manual\n" +
+                         "2 - Hill\n" +
+                         "3 - Cardio\n" +
+                         "4 - Strength\n" +
+                         "5 - User\n");
+
       choice = reader.nextInt();
 
-      System.out.println("Please select a duration\n");
-      duration = reader.nextInt();
-
-      System.out.println("Please select a difficulty level\n");
-      difficulty = reader.nextInt();
-
-      wo1.set_duration(duration);
-      wo1.set_resistence_level(difficulty);
-
-      wo1.start_wo();
-
-      int n = reader.nextInt();
-
-      while(wo1.check_finished()) {
-          wo1.shout_out();
-          try {
-              Thread.sleep(1000);
-          } catch (InterruptedException e) {
-              e.printStackTrace();
-          }
+      switch (choice) {
+         case 1:  wo = new Workout(); break;
+         case 2:  wo = new Hill_Workout(); break;
+         case 3:  wo = new Cardio_Workout(); break;
+         case 4:  wo = new Strength_Workout(); break;
+         case 5:  wo = new Workout(); break;
+         default: System.out.println("Invalid selection!");
       }
 
-      wo1.finish();
-
-      // while(true) {
-      //
-      //    if(n == 2) {
-      //       wo1.shout_out();
-      //    }
-      //    else if(n == 1) {
-      //       wo1.finish();
-      //       break;
-      //    }
-      //    else {
-      //       System.out.println("Try again");
-      //    }
-      //    n = reader.nextInt();
-      // }
+      wo.start_wo();
+      wo.run_wo();
+      wo.finish_wo();
    }
 
-    public static class Workout_timer extends Thread {
+    public class Workout_manager extends JLabel implements ActionListener {
 
-       int time     = 0;
+       int cals_burned         = 0;
+       int resistence_level    = 0;
+       int time                = 0;
+       int time_step           = 0;
+       float cadance           = 0;
+       float current_rate      = 0;
+       float distance          = 0;
+       float gear_ratio        = 0;
+       float old_rate          = 0;
+       float previous_distance = 0;
+       float watts_made        = 0;
+       //  int avg_hbm;
+       String display_time;
 
-        @Override
+      //   @Override
         public void run() {
             while (true) {
                 try {
+                    get_rpms();
+                    calculate_stats();
+
+                    display_time = String.format("%2d:%2d",
+                       TimeUnit.SECONDS.toMinutes(time),
+                       time - TimeUnit.SECONDS.toMinutes(time) * 60
+                    );
+
+                    System.out.format("\r\033[K%-5s %-5.2f mi   %3.1f mph %-5d", display_time, distance, current_rate, resistence_level);
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
-                    e.printStackTrace();
+                   // Anticipating the inturruption for exit
+                   System.out.print("\r\033[K");
                 }
                 time++;
+                time_step++;
+
+               Timer t = new Timer(1000, this);
+               t.start();
             }
         }
 
-        public void speak() {
-           System.out.print("\r" + time);
+// ----------------------------- setter methods ----------------------------- //
+        public void set_resistence(int level) {resistence_level = level;}
+// -------------------------- End setter methods ---------------------------- //
+
+// ----------------------------- getter methods ----------------------------- //
+        public int get_time() {return time;}
+        public float get_distance() {return distance;}
+// -------------------------- End getter methods ---------------------------- //
+
+// --------------------------- Private methods ------------------------------ //
+        private void calculate_stats() {
+            switch (resistence_level) {
+               case 1:  gear_ratio = (float) 39/23; set_gear_ratio(39,23); break;
+               case 2:  gear_ratio = (float) 39/21; set_gear_ratio(39,21); break;
+               case 3:  gear_ratio = (float) 39/19; set_gear_ratio(39,19); break;
+               case 4:  gear_ratio = (float) 50/23; set_gear_ratio(50,23); break;
+               case 5:  gear_ratio = (float) 39/18; set_gear_ratio(39,18); break;
+               case 6:  gear_ratio = (float) 39/17; set_gear_ratio(39,17); break;
+               case 7:  gear_ratio = (float) 50/21; set_gear_ratio(50,21); break;
+               case 8:  gear_ratio = (float) 39/16; set_gear_ratio(39,16); break;
+               case 9:  gear_ratio = (float) 39/15; set_gear_ratio(39,15); break;
+               case 10: gear_ratio = (float) 50/19; set_gear_ratio(50,19); break;
+               case 11: gear_ratio = (float) 50/18; set_gear_ratio(50,18); break;
+               case 12: gear_ratio = (float) 39/14; set_gear_ratio(39,14); break;
+               case 13: gear_ratio = (float) 50/17; set_gear_ratio(50,17); break;
+               case 14: gear_ratio = (float) 39/13; set_gear_ratio(39,13); break;
+               case 15: gear_ratio = (float) 50/16; set_gear_ratio(50,16); break;
+               case 16: gear_ratio = (float) 39/12; set_gear_ratio(39,12); break;
+               case 17: gear_ratio = (float) 50/15; set_gear_ratio(50,15); break;
+               case 18: gear_ratio = (float) 50/14; set_gear_ratio(50,14); break;
+               case 19: gear_ratio = (float) 50/13; set_gear_ratio(50,13); break;
+               case 20: gear_ratio = (float) 50/12; set_gear_ratio(50,12); break;
+           }
+
+           // (Pi x diameter) x (gear ratio) x cadence
+           current_rate = 0.079f * gear_ratio * cadance;
+
+           // If the rate changes
+           if(current_rate != old_rate) {
+
+              // Restart the time_step clock
+              time_step = 0;
+              old_rate  = current_rate;
+              previous_distance = distance;
+           }
+
+           distance = previous_distance + (current_rate * (time_step/3600f));
         }
 
-        public int get_time() {
-           return time;
+        private void set_gear_ratio(int front_gear, int back_gear) {
+            try {
+               Runtime.getRuntime().exec("/home/mmost/Documents/test/se_460/change_gear_ratio " + front_gear + " " + back_gear);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
+        private void get_rpms() {
+           JSONParser parser = new JSONParser();
+
+           try {
+               JSONObject car_inputs = (JSONObject) parser.parse(new FileReader("/home/mmost/Documents/test/se_460/car_inputs.json"));
+               cadance = (long) car_inputs.get("rpms");
+           } catch (FileNotFoundException e) {
+               e.printStackTrace();
+           } catch (IOException e) {
+               e.printStackTrace();
+           } catch (ParseException e) {
+               e.printStackTrace();
+           }
+        }
+
+        public void actionPerformed(ActionEvent ae) {
+          Date d = new Date();
+         //  setText(sdf.format(d));
+        }
+    }
+
+    class ClockLabel extends JLabel implements ActionListener {
+
+      String type;
+      SimpleDateFormat sdf;
+
+      public ClockLabel(String type) {
+        this.type = type;
+        setForeground(Color.green);
+
+        switch (type) {
+          case "date" : sdf = new SimpleDateFormat("  MMMM dd yyyy");
+                        setFont(new Font("sans-serif", Font.PLAIN, 12));
+                        setHorizontalAlignment(SwingConstants.LEFT);
+                        break;
+          case "time" : sdf = new SimpleDateFormat("hh:mm:ss a");
+                        setFont(new Font("sans-serif", Font.PLAIN, 40));
+                        setHorizontalAlignment(SwingConstants.CENTER);
+                        break;
+          case "day"  : sdf = new SimpleDateFormat("EEEE  ");
+                        setFont(new Font("sans-serif", Font.PLAIN, 16));
+                        setHorizontalAlignment(SwingConstants.RIGHT);
+                        break;
+          default     : sdf = new SimpleDateFormat();
+                        break;
+        }
+
+        Timer t = new Timer(1000, this);
+        t.start();
+      }
+
+      public void actionPerformed(ActionEvent ae) {
+        Date d = new Date();
+        setText(sdf.format(d));
+      }
     }
 }
